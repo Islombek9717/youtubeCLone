147221c29da0ecbb4c5b2943c5a9148dca52199e
Islombek <sultonovislom9717@gmail.com>
2022-01-05 12:18:21 +0500

    first commit

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+
+import { Main } from "./style";
+import BodyTop from "./BodyTop/BodyTop";
+import { MovieContext } from "../../context/movie";
+
+const Body = () => {
+  const [state] = useContext(MovieContext);
+  return (
+    <Main>
+      <BodyTop />
+      <Main.Content>
+        {state.map((value) => {
+          return (
+            <Main.Channel key={value.id}>
+              <img src={value.img} alt="" width={295} height={186} />
+              <Main.User>
+                <img src={value.user} alt="" />
+                <Main.ChannelTitle>{value.title}</Main.ChannelTitle>
+              </Main.User>
+              <Main.Description>{value.description}</Main.Description>
+              <Main.Views>
+                {value.id ** 2 * 2 + 1}k views -{" "}
+                {Math.round((value.id * 100) / value.id ** 2)} days ago{" "}
+              </Main.Views>
+            </Main.Channel>
+          );
+        })}
+      </Main.Content>
+    </Main>
+  );
+};
+
+export default Body;
---
+++ src/components/Body/BodyTop/BodyTop.jsx
@@ -0,0 +1,20 @@
+import { Category } from "./style";
+
+const BodyTop = () => {
+  return (
+    <Category>
+      <Category.Btn>All</Category.Btn>
+      <Category.Btn>Comedy</Category.Btn>
+      <Category.Btn>Game</Category.Btn>
+      <Category.Btn>Vlog</Category.Btn>
+      <Category.Btn>Music</Category.Btn>
+      <Category.Btn>Movie</Category.Btn>
+      <Category.Btn>Course</Category.Btn>
+      <Category.Btn>Mixes</Category.Btn>
+      <Category.Btn>React</Category.Btn>
+      <Category.Btn>Javascript</Category.Btn>
+    </Category>
+  );
+};
+
+export default BodyTop;
